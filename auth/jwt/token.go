e2a2b060b2fe21cc7ef7c15844ceda336f2d5f29
Fallensouls <122584482@qq.com>
2019-03-19 13:35:28 +0800

    refactoring jwt-base auth and add explanation to readme

--- auth/jwt/token.go
+++ auth/jwt/token.go
@@ -5,6 +5,7 @@ package jwt
 import (
 	"errors"
 	Jwt "github.com/dgrijalva/jwt-go"
+	"log"
 	"net/http"
 )
 
@@ -23,6 +24,7 @@ var (
 	ErrInvalidSigningMethod = errors.New("JWT: invalid JWT signing method")
 	ErrInvalidDuration      = errors.New("JWT: duration of jwt can not be less than or equal to zero")
 	ErrTokenNotFound        = errors.New("JWT: there is no token in the given header")
+	ErrNoHeader             = errors.New("JWT: there is no specified header which contains a token")
 	//ErrTokenRevoked      	 =  errors.New("JWT: your token has been revoked")
 )
 
@@ -72,6 +74,10 @@ func NewTokenConfig(options Options, store Store) (*Token, error) {
 		return nil, ErrInvalidDuration
 	}
 
+	if options.Header == "" {
+		return nil, ErrNoHeader
+	}
+
 	return &Token{privateKey, publicKey, options, store}, nil
 }
 
@@ -105,9 +111,17 @@ func (t *Token) CheckToken(token string) (*UserInfo, error) {
 	if err != nil {
 		return nil, err
 	}
+	// When there is no storage, we would like to return information from token as UserInfo.
+	if t.store == nil {
+		return &UserInfo{tokenInfo.Id, tokenInfo.Data}, nil
+	}
 	return t.store.Check(tokenInfo.Id, tokenInfo.IssuedAt)
 }
 
 func (t *Token) RevokeToken(id string) error {
+	// When there is no storage, no token would be revoked.
+	if t.store == nil {
+		log.Panicf("JWT: no storage provided, please check your storage setting")
+	}
 	return t.store.Revoke(id)
 }
